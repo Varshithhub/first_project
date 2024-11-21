1a6bef714f73014b23b9fc99f7c38c9ef945f1d3
Varshithhub <varshithlavanya67@gmail.com>
2024-11-22 01:07:26 +0530

    first commit

--- Game.js
+++ Game.js
@@ -1,7 +1,7 @@
 
 
-
-
+let userscore;
+let roundcount;
 
 function playGame(playerchoice){
     let arr = ["rock" , "paper" , "scissors"];
@@ -14,11 +14,33 @@ function playGame(playerchoice){
         (playerchoice === "paper" && arr[index] === "rock") ||
         (playerchoice === "scissors" && arr[index] === "paper")
       )){
-        resultmsg=`you win ${playerchoice} beats ${arr[index]}`
+        resultmsg=`you win ${playerchoice} beats ${arr[index]}`;
+        userscore++;
       }
     else{
         resultmsg = `you lose ${arr[index]} beats ${playerchoice}`
     }  
-
+    roundcount++;
     document.getElementById("result").innerText = resultmsg;
+    document.getElementById("score").innerText = `score : ${userscore}`;
+    document.getElementById("round").innerText = `round : ${roundcount}/10`;
+    if(roundcount===10){
+      endgame();
+    }
+
+}
+
+function endgame(){
+  const finalmsg = 
+        userscore>=6?`Congratulations! You won the game with a score of ${userscore} out of 10!`:
+        `Game over! You lost with a score of ${userscore} out of 10.`;
+        document.getElementById("result").innerText =finalmsg;
+  document.getElementById("resbtn").style.display="block";      
+}
+
+
+function restart(){
+  userscore =0 ;
+  roundcount=0;
+  document.getElementById("resbtn").style.display="none";
 }
